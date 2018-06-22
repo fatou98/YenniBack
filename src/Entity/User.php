8556bc0d5930bffd11168eaa4c41c8453ec53712
fatou98 <mbathiefatou601@gmail.com>
2018-06-22 21:56:24 +0000

    Creation de la table User

---
+++ src/Entity/User.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nomcomplet;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numpiece;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $tel;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $password;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNomcomplet(): ?string
+    {
+        return $this->nomcomplet;
+    }
+
+    public function setNomcomplet(string $nomcomplet): self
+    {
+        $this->nomcomplet = $nomcomplet;
+
+        return $this;
+    }
+
+    public function getNumpiece(): ?int
+    {
+        return $this->numpiece;
+    }
+
+    public function setNumpiece(int $numpiece): self
+    {
+        $this->numpiece = $numpiece;
+
+        return $this;
+    }
+
+    public function getTel(): ?int
+    {
+        return $this->tel;
+    }
+
+    public function setTel(int $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
