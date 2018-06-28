6c3316f3089a3b354cfc068b5fd0d2f865c0055e
Gueye1890 <jaafargueye@gmail.com>
2018-06-28 23:39:43 +0000

    modification mo

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,69 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Form\UserType;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends Controller
-{
+class SecurityController extends Controller {
+
     /**
-     * @Route("/security", name="security")
+     * @Route("/inscription",name="inscrire")
      */
-    public function index()
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            //on active par défaut
+            $user->setIsActive(true);
+            $user->addRole("ROLE_ADMIN");
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+            $this->addFlash('success', 'Votre compte à bien été enregistré.');
+            return $this->redirectToRoute('connexion');
+        }
+        return $this->render('security/inscription.html.twig', ['form' => $form->createView(), 'mainNavRegistration' => true, 'title' => 'Inscription']);
+    }
+     /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function login(Request $request, AuthenticationUtils $authenticationUtils) {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        //
+        $form = $this->get('form.factory')
+                ->createNamedBuilder(null)
+                ->add('_username', null, ['label' => 'Email'])
+                ->add('_password', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class, ['label' => 'Mot de passe'])
+                ->add('connexion', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Connexion', 'attr' => ['class' => 'green darken-4']])
+                ->getForm();
+//return $this->redirectToRoute('membre');
+
+        return $this->render('security/connexion.html.twig', [
+                    'mainNavLogin' => true, 'title' => 'Connexion',
+                    //
+                    'form' => $form->createView(),
+                    'last_username' => $lastUsername,
+                    'error' => $error,
         ]);
     }
+
+
+
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,108 +1,266 @@
 <?php
-
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="user")
+ * @UniqueEntity(fields="email")
+ * @ORM\Entity()
  */
-class User
-{
+class User implements AdvancedUserInterface, \Serializable {
+
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\Id
      * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Column(type="string", length=100, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+ /**
+     * @ORM\Column(type="string", length=30, unique=false)
+     * @Assert\NotBlank()
      */
     private $nomcomplet;
-
-    /**
-     * @ORM\Column(type="integer")
+ /**
+     * @ORM\Column(type="integer", length=50, unique=true)
+     * @Assert\NotBlank()
      */
-    private $numpiece;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $tel;
-
-    /**
-     * @ORM\Column(type="string", length=255)
+    private $Numpiece;
+/**
+     * @ORM\Column(type="string", length=255, unique=false)
+     * @Assert\NotBlank()
      */
     private $adresse;
+ /**
+     * @ORM\Column(type="integer", length=50, unique=true)
+     * @Assert\NotBlank()
+     */
+    private $Tel;
 
     /**
-     * @ORM\Column(type="string", length=20)
+     * @Assert\NotBlank()
+     * @Assert\Length(max=250)
+     */
+    private $plainPassword;
+
+    /**
+     * The below length depends on the "algorithm" you use for encoding
+     * the password, but this works well with bcrypt.
+     *
+     * @ORM\Column(type="string", length=64)
      */
     private $password;
 
-    public function getId()
-    {
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(name="roles", type="array")
+     */
+    private $roles = array();
+
+    public function __construct() {
+        $this->isActive = true;
+        // may not be needed, see section on salt below
+        // $this->salt = md5(uniqid('', true));
+    }
+
+    public function getUsername() {
+        return $this->email;
+    }
+
+    public function getSalt() {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function getRoles() {
+        if (empty($this->roles)) {
+            return ['ROLE_USER'];
+        }
+        return $this->roles;
+    }
+
+    function addRole($role) {
+        $this->roles[] = $role;
+    }
+
+    public function eraseCredentials() {
+        
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize() {
+        return serialize(array(
+            $this->id,
+            $this->email,
+            $this->password,
+            $this->isActive,
+                // see section on salt below
+                // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized) {
+        list (
+                $this->id,
+                $this->email,
+                $this->password,
+                $this->isActive,
+                // see section on salt below
+                // $this->salt
+                ) = unserialize($serialized);
+    }
+
+    public function isAccountNonExpired() {
+        return true;
+    }
+
+    public function isAccountNonLocked() {
+        return true;
+    }
+
+    public function isCredentialsNonExpired() {
+        return true;
+    }
+
+    public function isEnabled() {
+        return $this->isActive;
+    }
+
+    function getId() {
         return $this->id;
     }
 
-    public function getNomcomplet(): ?string
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getPlainPassword() {
+        return $this->plainPassword;
+    }
+
+    function getIsActive() {
+        return $this->isActive;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setPlainPassword($plainPassword) {
+        $this->plainPassword = $plainPassword;
+    }
+
+    function setIsActive($isActive) {
+        $this->isActive = $isActive;
+    }
+
+
+    /**
+     * Get the value of Nomcomplet
+     */ 
+    public function getNomcomplet()
     {
         return $this->nomcomplet;
     }
 
-    public function setNomcomplet(string $nomcomplet): self
+    /**
+     * Set the value of Nomcomplet
+     *
+     * @return  self
+     */ 
+    public function setNomcomplet($nomcomplet)
     {
         $this->nomcomplet = $nomcomplet;
 
         return $this;
     }
 
-    public function getNumpiece(): ?int
+    /**
+     * Get the value of Numpiece
+     */ 
+    public function getNumpiece()
     {
-        return $this->numpiece;
+        return $this->Numpiece;
     }
 
-    public function setNumpiece(int $numpiece): self
+    /**
+     * Set the value of Numpiece
+     *
+     * @return  self
+     */ 
+    public function setNumpiece($Numpiece)
     {
-        $this->numpiece = $numpiece;
+        $this->Numpiece = $Numpiece;
 
         return $this;
     }
 
-    public function getTel(): ?int
-    {
-        return $this->tel;
-    }
-
-    public function setTel(int $tel): self
-    {
-        $this->tel = $tel;
-
-        return $this;
-    }
-
-    public function getAdresse(): ?string
+    /**
+     * Get the value of adresse
+     */ 
+    public function getAdresse()
     {
         return $this->adresse;
     }
 
-    public function setAdresse(string $adresse): self
+    /**
+     * Set the value of adresse
+     *
+     * @return  self
+     */ 
+    public function setAdresse($adresse)
     {
         $this->adresse = $adresse;
 
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * Get the value of Tel
+     */ 
+    public function getTel()
     {
-        return $this->password;
+        return $this->Tel;
     }
 
-    public function setPassword(string $password): self
+    /**
+     * Set the value of Tel
+     *
+     * @return  self
+     */ 
+    public function setTel($Tel)
     {
-        $this->password = $password;
+        $this->Tel = $Tel;
 
         return $this;
     }
 }
+
