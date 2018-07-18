93307ef9d01746139144dda9894166045b7fed32
Gueye1890 <jaafargueye@gmail.com>
2018-07-18 19:16:01 +0000

    modification mo

--- src/Controller/MedecinController.php
+++ src/Controller/MedecinController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
+use App\Form\ClientformType;
+use App\Repository\ClientRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\PriseDerendezvous;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
@@ -38,14 +43,12 @@ class MedecinController extends Controller
         ]);
     }
      
-       /**
-     * @Route("/liste", name="liste")
+    /**
+     * @Route("/listepatient", name="listepatient")
      */
-    public function listepatient()
+    public function listepatient(ClientRepository $clients)
     {
-        return $this->render('medecin/listepatient.html.twig', [
-            'controller_name' => 'MedecinController',
-        ]);
+        return $this->render('medecin/listepatient.html.twig', ['clients'=>$clients->findAll()]);
     }
         /**
      * @Route("/listerv", name="liste")
@@ -54,6 +57,59 @@ class MedecinController extends Controller
     {
         return $this->render('medecin/listervmedcin.html.twig', [
             'controller_name' => 'MedecinController',
+        ]);
+    }
+
+    /**
+     * @Route("patient/{id}", name="edit_patient")
+     */
+    public function edit(Request $request, Client $client): Response{
+        $form = $this->createForm(ClientformType::class, $client);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('listepatient');
+        }
+
+        return $this->render('medecin/edit.html.twig', [
+            'client'=>$client,
+            'form'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/delpatient/{id}", name="delete_patient")
+     */
+    public function delete(Request $request, Client $client): Response
+    {
+        
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($client);
+            $em->flush();
+        
+        return $this->redirectToRoute('listepatient');
+    }
+    /**
+     * @Route("/addpatient", name="add_patient", methods="GET|POST")
+     */
+    public function add(Request $request){
+
+        $client = new Client();
+        $form = $this->createForm(ClientformType::class, $client);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($client);
+            $em->flush();
+
+            return $this->redictToRoute('listepatient');
+        }
+
+        return $this->render('medecin/addpatient.html.twig', [
+            'client'=>$client,
+            'form'=> $form->createView()
         ]);
     }
 }
